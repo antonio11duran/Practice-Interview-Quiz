b68cd52aa6b19fc933403f4331dd975679ae236a
Duran11 <antonio11duran@gmail.com>
2022-09-07 08:15:24 -0600

    added timer function

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,20 @@
+var timer = document.querySelector(".timer");
 var title = document.getElementById(".title");
 var question = document.getElementById(".question");
+
+var timeLeft = 60;
+
+// timer works, need to connect to start with start button press
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timer.textContent = "Timer: " + timeLeft;
+
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+// delete this function to stop automatic countdown on page load
+setTime();
