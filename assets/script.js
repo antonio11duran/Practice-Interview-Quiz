d37f29b3f00cc381d1290d16ca0cad135122ea0d
Duran11 <antonio11duran@gmail.com>
2022-09-09 11:00:53 -0600

    added ul elements

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
-var timer = document.querySelector(".timer");
-var title = document.getElementById(".title");
-var question = document.getElementById(".question");
+var timer = document.querySelector("timer");
+var title = document.getElementById("title");
+var question = document.getElementById("question");
+var listEl = document.getElementById("listEl")
+var startButton = document.querySelector(".start-button");
 
 var timeLeft = 60;
 
 // timer works, need to connect to start with start button press
 function setTime() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         timeLeft--;
         timer.textContent = "Timer: " + timeLeft;
 
@@ -16,5 +18,26 @@ function setTime() {
     }, 1000);
 }
 
-// delete this function to stop automatic countdown on page load
-setTime();
+function questionOne() {
+    setTime();
+
+    // startButton.disabled = true;
+
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    var li3 = document.createElement("li");
+    var li4 = document.createElement("li");
+
+    li1.textContent = "a";
+    li2.textContent = "b";
+    li3.textContent = "c";
+    li4.textContent = "d";
+
+    listEl.appendChild(li1);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+    listEl.appendChild(li4);
+}
+
+// put setTime as a filler function. replace with quiz function once created.
+startButton.addEventListener('click', questionOne);
