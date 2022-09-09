3a9f6744a9ae982d386b6cc4b7a058aa06239e1a
Antonio <antonio11duran@gmail.com>
2022-09-09 15:49:36 -0600

    added high score files & most quiz js is complete

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
-var timer = document.querySelector("timer");
-var title = document.getElementById("title");
-var question = document.getElementById("question");
-var listEl = document.getElementById("listEl")
-var startButton = document.querySelector(".start-button");
+var timer = document.getElementById("timer");
+var quizContainer = document.getElementById("quizContainer");
+var listEl = document.getElementById("listEl");
+var startButton = document.getElementById("start-button");
+var rightWrong = document.getElementById("rightWrong");
 
-var timeLeft = 60;
+var timeLeft = 101;
+var score = 0;
+var qindex = 0;
+var penalty = 5;
+
+var quizQuestions = [
+    {
+        question: "Answer is b",
+        answers: ["a", "b", "c", "d"],
+        correct: "b"
+    },
+    {
+        question: "Answer is b",
+        answers: ["a", "b", "c", "d"],
+        correct: "b"
+    },
+    {
+        question: "Answer is b",
+        answers: ["a", "b", "c", "d"],
+        correct: "b"
+    }
+];
 
 // timer works, need to connect to start with start button press
 function setTime() {
@@ -14,30 +35,88 @@ function setTime() {
 
         if (timeLeft === 0) {
             clearInterval(timerInterval);
+            endGame();
         }
     }, 1000);
+    createQ(qindex);
 }
 
-function questionOne() {
-    setTime();
+var ulEl = document.createElement("ul")
 
-    // startButton.disabled = true;
+function createQ(qindex) {
+    quizContainer.innerHTML = "";
+    ulEl.innerHTML = "";
 
-    var li1 = document.createElement("li");
-    var li2 = document.createElement("li");
-    var li3 = document.createElement("li");
-    var li4 = document.createElement("li");
+    for (var i = 0; i < quizQuestions.length; i++) {
+        var quizQ = quizQuestions[qindex].question;
+        var quizA = quizQuestions[qindex].answers;
+        quizContainer.textContent = quizQ;
+    }
 
-    li1.textContent = "a";
-    li2.textContent = "b";
-    li3.textContent = "c";
-    li4.textContent = "d";
-
-    listEl.appendChild(li1);
-    listEl.appendChild(li2);
-    listEl.appendChild(li3);
-    listEl.appendChild(li4);
+    quizA.forEach(function (createLi) {
+        var quizList = document.createElement("li");
+        quizList.textContent = createLi;
+        quizContainer.appendChild(ulEL);
+        ulEl.appendChild(quizList);
+        createLi.addEventListener('click', (compare));
+    })
 }
 
-// put setTime as a filler function. replace with quiz function once created.
-startButton.addEventListener('click', questionOne);
+function correctAnswer(event) {
+    var choice = event.target;
+
+    if (choice.matches("li")) {
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "newDiv");
+
+        if (choice.textContent === quizQuestions[qindex].correct) {
+            score + 1;
+            newDiv.textContent = "Correct Answer. Good job!"
+        } else {
+            timeLeft = timeLeft - penalty;
+            newDiv.textContent = "Wrong answer. Time deducted."
+        }
+    }
+
+    qindex++;
+
+    if (qindex >= quizQuestions.length) {
+        endGame();
+        newDiv.textContent = "Quiz over. You scored " + score + " points!";
+    } else {
+        createQ(qindex);
+    }
+    quizContainer.appendChild(newDiv);
+}
+
+function endGame() {
+    quizContainer.innerHTML = "";
+    ulEl.innerHTML = "";
+
+    var title = document.getElementById("title");
+    title.textContent = "Quiz Over!";
+
+    
+    var initials = document.createElement("label");
+    initials.setAttribute("id", "initials");
+    initials.textContent = "Please enter your initials: ";
+    
+    var inputBox = document.createElement("input");
+    inputBox.setAttribute("id", "inputBox");
+    inputBox.setAttribute("type", "text");
+    inputBox.textContent = "";
+
+    var submitBox = document.createElement("button");
+    submitBox.setAttribute("id", "submitBox");
+    submitBox.setAttribute("type", "submit");
+    submitBox.textContent = "Submit Score";
+
+    quizContainer.appendChild(title);
+    quizContainer.appendChild(initials);
+    quizContainer.appendChild(inputBox);
+    quizContainer.appendChild(submitBox);
+
+    submitBox.addEventListener('click', function() {
+        
+    })
+}
